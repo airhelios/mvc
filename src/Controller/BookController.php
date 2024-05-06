d42f7633ee26f6625ae3f1fb025f407644d8f727
owsu23 <owsu23@student.bth.se>
2024-05-06 22:34:49 +0200

    Creating update/show one /show all views

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -10,8 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Service\FileUploader;
 
 
-
-
 use App\Entity\Book;
 use App\Repository\BookRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -53,11 +51,78 @@ class BookController extends AbstractController
 
             $entityManager->persist($book);
             $entityManager->flush();
-            return new Response("Book with title " . $book->getTitle() . " has been created!");
+            return new Response("Book with title " . $book->getTitle() . " and id" . $book->getId() . " has been created!");
         }
         return $this->render('book/create.html.twig', [
             'book_form' => $form->createView()
         ]);
+    }
 
+    #[Route('/library/update/{id}', name: 'update_book')]
+    public function updateBook(
+        Request $request,
+        ManagerRegistry $doctrine,
+        FileUploader $fileUploader,
+        BookRepository $bookRepository,
+        int $id)
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $entityManager->getRepository(Book::class)->find($id);
+
+        $form = $this->createForm(BookFormType::class, $book);
+        $form->handleRequest($request);
+
+
+
+        $entityManager = $doctrine->getManager();
+        // dd("HEEEEEJ");
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book->setTitle($form->get('title')->getData());
+            $book->setAuthor($form->get('author')->getData());
+            $book->setISBN($form->get('ISBN')->getData());
+            // dd($form->get('title')->getData());
+   
+
+            $imgFile = $form->get('img-upload')->getData();
+
+            // this condition is needed because the 'img-upload' field is not required
+            // so the img file must be processed only when a file is uploaded
+            if ($imgFile) {
+                $img = $form->get('img-upload')->getData();
+                if ($img) {
+                    $imgFileName = $fileUploader->upload($img);
+                    $book->setImg($imgFileName);
+                }
+            }
+
+            // $entityManager->persist($book);
+            $entityManager->flush();
+        }
+        return $this->render('book/update.html.twig', [
+            'book' => $book,
+            'book_form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/library/show/{id}', name: 'book_by_id')]
+    public function showBookById(
+        BookRepository $bookRepository,
+        int $id
+    ): Response {
+        $book = $bookRepository
+            ->find($id);
+
+        return $this->render('book/show_one.html.twig', [
+                'book' => $book]);
+    }
+
+    #[Route('/library/show', name: 'book_show_all')]
+    public function showAllBook(
+        BookRepository $bookRepository
+    ): Response {
+        $books = $bookRepository
+            ->findAll();
+
+        return $this->json($books);
     }
 }
